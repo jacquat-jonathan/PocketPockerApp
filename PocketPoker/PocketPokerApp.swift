fabc72443b3d83536c8cb9ab96f82c215dfe3491
Jacquat Jonathan <jacquat.jonathan@gmail.com>
2024-10-20 16:43:26 +0200

    Initial Commit

---
+++ PocketPoker/PocketPokerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PocketPokerApp.swift
+//  PocketPoker
+//
+//  Created by Jonathan Jacquat on 20.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct PocketPokerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PocketPokerTests/PocketPokerTests.swift
@@ -0,0 +1,17 @@
+//
+//  PocketPokerTests.swift
+//  PocketPokerTests
+//
+//  Created by Jonathan Jacquat on 20.10.2024.
+//
+
+import Testing
+@testable import PocketPoker
+
+struct PocketPokerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
